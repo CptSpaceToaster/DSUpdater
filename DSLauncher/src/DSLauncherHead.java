5069df404123bc6e4994737a573503a34a6f5201
CptSpaceToaster <Lugia978@yahoo.com>
2013-10-31 10:50:55 -0400

    Started Reading Update.txt

--- DSLauncher/src/DSLauncherHead.java
+++ DSLauncher/src/DSLauncherHead.java
@@ -9,8 +9,11 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -27,39 +30,67 @@ public class DSLauncherHead extends JFrame {
 	/** Auto Generated ID **/
 	private static final long serialVersionUID = 4255942636165851766L;
 	
-	/** Title Text **/
+	/* GUI Stuff */
 	private JLabel titleLabel;
-	
-	/** JLabel for a scrolling status indicator **/
 	private JLabel statusLabel;
-	
-	/** String for the Status message **/
-	private String statusString;
-
-	/** Scroll Pane to mount statusLabel **/
 	private JScrollPane scrollPane;
-	
-	/** JPanel to hold the scroll Pane **/
 	private JPanel statusPanel;
 	
+	/* Instance Variables */
+	private URL updateUrl;
+	
+	private String statusString;
 	private String versionFromFile;
 	private String versionFromServer;
-		
-	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
-	private final String DEFAULT_FILE_NAME = "DSLauncher.properties";
 	
-	private final int DEFAULT_WIDTH = 400;
-	private final int DEFAULT_HEIGHT = 300;
+	private int updateIndex;
 	
+	private ArrayList<String> versions;
+	private ArrayList<String> downloadUrls; 
+	private ArrayList<String> fileNames;
+	
+	/* Comparison Definitions */
 	private static final int GREATER = 1;
 	private static final int EQUAL = 0;
 	private static final int OUT_OF_DATE = -1;
+	
+	
+	
+	/***********************/
+	/* START CONFIGURATION */
+	/***********************/
+	
+	/** Location of update.txt **/
+	private final String UPDATE_URL_STRING = "https://dl.dropboxusercontent.com/u/5921811/update.txt";
+	
+	/** File Date Format **/
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
+	
+	/** DS Update Log Filename (The Date will be Appended onto the end of this **/
+	private final String UPDATE_FILENAME = "DS Update Log ";
+	
+	/** DS Error Log Filename (The Date will be Appended onto the end of this **/
+	private final String ERROR_FILENAME = "DS Error Log ";
+	
+	/** DS Launcher Properties Filename **/
+	private final String DEFAULT_FILE_NAME = "DSLauncher.properties";
+	
+	/** Pixel Width of Window **/
+	private final int DEFAULT_WIDTH = 400;
+	
+	/** Pixel Height of Window **/
+	private final int DEFAULT_HEIGHT = 300;
+	
+	/*********************/
+	/* END CONFIGURATION */
+	/*********************/
+	
 	
 	/** 
 	 * Main method to be called externally.  Creates an instance of DSLauncherHead, which starts off this carnival ride!
 	 * 
 	 * @param args Command Line Arguments... currently unused
-	 * TODO: Consider making a CLA to start the jar with the terminal in the background
+	 * TODO: Consider making a CLA to start the jar with the terminal in the background, or always log.
 	 */
 	public static void main(String[] args) {
 		new DSLauncherHead();
@@ -83,7 +114,7 @@ public class DSLauncherHead extends JFrame {
 			appendLine("Server Version: " + versionFromServer);
 			appendLine("Local Version: " + versionFromFile);
 			
-			saveConsoleLog("DSUpdate Log " + dateFormat.format(new Date()) + ".txt");
+			saveConsoleLog(UPDATE_FILENAME + dateFormat.format(new Date()) + ".txt");
 			JOptionPane.showMessageDialog(null, "You appear to be a couple versions ahead of us.\nDid you modify " + DEFAULT_FILE_NAME + "?", "Version Mismatch", JOptionPane.WARNING_MESSAGE);
 			closeGUI();
 		}
@@ -95,7 +126,7 @@ public class DSLauncherHead extends JFrame {
 			updateDSMinecraftInstallation();
 			saveToTextFile(DEFAULT_FILE_NAME);
 			
-			saveConsoleLog("DSUpdate Log " + dateFormat.format(new Date()) + ".txt");
+			saveConsoleLog(UPDATE_FILENAME + dateFormat.format(new Date()) + ".txt");
 			
 			JOptionPane.showMessageDialog(null, "Your version was updated to " + versionFromServer, "Updated", JOptionPane.PLAIN_MESSAGE);
 			
@@ -137,11 +168,13 @@ public class DSLauncherHead extends JFrame {
 		//The X doesn't exist any more, but I set the default close operation anyways...
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-		
+		//Start window in the center of the screen
 		setLocationRelativeTo(null);
-		
+			
 		//Do we want users to be able to resize the window/popup? 
 		//setResizable(false);
+		
+		
 	}
 	
 	/**
@@ -153,10 +186,17 @@ public class DSLauncherHead extends JFrame {
 	}
 	
 	/**
-	 * An Unused empty Post Initialization routine
+	 * Instantiates URL's for updater.  Currently uses a "hard set" URL string to instantiate the URL, but we 
+	 * could use the text file as well.
 	 */
 	private void postInit() {
-		
+		try {
+			updateUrl = new URL(UPDATE_URL_STRING);
+		} catch (MalformedURLException e) {
+			//Uhhhh... it broke?
+			appendLine("Error: URL could not be made");
+			saveConsoleLog(ERROR_FILENAME + dateFormat.format(new Date()) + ".txt");
+		}
 	}
 	
 	private void closeGUI() {
@@ -166,13 +206,13 @@ public class DSLauncherHead extends JFrame {
 	
 	private void loadFromTextFile(String filename){
 		try {
-			Scanner scanner = new Scanner(new File(filename));
+			Scanner s = new Scanner(new File(filename));
 			
-			versionFromFile = scanner.nextLine().trim();
+			versionFromFile = s.nextLine().trim();
 			//TODO: Read more data than just the version number?
 
 			
-			scanner.close();
+			s.close();
 		} catch (FileNotFoundException ex) {
 			//No file exists, program will make a new File, and set default values
 			//TODO: Make necessary instance variables, and set them properly here
@@ -207,7 +247,8 @@ public class DSLauncherHead extends JFrame {
 	private void saveConsoleLog(String filename) {
 		try {
 			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
-			String output = statusLabel.getText().replace("<br>", "\r\n");	//Windows Specific Output Text
+			//TODO: Not use Windows Specific NewLine Character \r\n
+			String output = statusLabel.getText().replace("<br>", "\r\n");
 			output = output.replace("<html>", "");
 			output = output.replace("</html>", "");
 			
@@ -222,15 +263,40 @@ public class DSLauncherHead extends JFrame {
 	
 	private void updateVersionFromServer() {
 		appendLine("Obtaining version from server.");
-		int pingResponse;
-		
-		//TODO Actually get the Version from the Server
-		versionFromServer = "1.0.0";
-		pingResponse = -1;
-		
-		
-		
-		appendLine("Recieved server version reply: " + versionFromServer + ", " + pingResponse + "ms later.");
+		try {
+			Scanner s = new Scanner(updateUrl.openStream());
+			updateIndex = 0;
+			versionFromServer = "";
+			
+			String line = s.nextLine().replace(" ", "");  
+			while (line != null)  
+			{  
+			    
+				//Splits the string into parts based on semi colons, and creates empty strings if there exists ;;
+			    String[] updateParts = line.split(";", -1);
+			    try {
+			    	versions.add(updateParts[0]);
+			    	downloadUrls.add(updateParts[1]);
+			    	fileNames.add(updateParts[2]);
+			    } catch(IndexOutOfBoundsException e) { 
+					appendLine("Update.txt doesn't look right.");
+					appendLine("expected: Version; DownloadURL; FileName");
+			    }
+			    
+			    //If version from update.txt is greater than the currently largest version we have, then overwrite it!
+			    if(compareVersion(versions.get(updateIndex), versionFromServer) == GREATER) {
+			    	versionFromServer = versions.get(updateIndex);
+			    }
+			    
+			    line = s.nextLine().trim();
+			    updateIndex ++;
+			}
+			appendLine("Recieved server version reply: " + versionFromServer);
+			s.close();
+		} catch(IOException e) {
+			appendLine("Couldn't recieve response from server, ignoring updates");
+			versionFromServer = versionFromFile;
+		}
 	}
 	
 	private int compareVersion(String vf, String vs) {
